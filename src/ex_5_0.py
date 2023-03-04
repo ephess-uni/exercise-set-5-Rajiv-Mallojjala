85bb604560db0ee888447300c090df5c6c876af5
Rajiv-Mallojjala <61338297+Rajiv-Mallojjala@users.noreply.github.com>
2023-03-03 19:58:35 -0600

    Update ex_5_0.py
    Updated ex_5_0

--- src/ex_5_0.py
+++ src/ex_5_0.py
@@ -2,11 +2,14 @@
 
 
 def line_count(infile):
-    pass
+    with open(infile) as f:
+        lines = f.readlines()
+        num_lines = len(lines)
+        print("Number of lines in file {}: {}".format(infile, num_lines))
 
 
 if __name__ == "__main__":
-    # get the utility function for path discovery
+    line_count("example.txt")
     try:
         from src.util import get_repository_root
     except ImportError:
