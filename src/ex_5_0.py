d1e672345818b4ecc2324b0eb3f998efe4ca70e3
Rajiv-Mallojjala <61338297+Rajiv-Mallojjala@users.noreply.github.com>
2023-03-04 01:35:00 -0600

    Updated

--- src/ex_5_0.py
+++ src/ex_5_0.py
@@ -4,7 +4,7 @@ def line_count(infile):
     print(len(line))
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     # get the utility function for path discovery
     try:
         from src.util import get_repository_root
